6bcad432a57780fb64028643367afbc9f9ef11bd
Shinya Todaka <tdk0105.strawberry@gmail.com>
2020-10-29 15:22:27 +0900

    Initial Commit

---
+++ todo-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo-iOS
+//
+//  Created by 戸高新也 on 2020/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todo-iOS/todo_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_iOSApp.swift
+//  todo-iOS
+//
+//  Created by 戸高新也 on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct todo_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
